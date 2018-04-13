d61b813b51c35710ec5fbf86d83b3e0350286705
赵晓冬 <zhaoxiaodong@hujiang.com>
2018-04-13 18:37:28 +0800

    feat: add cache

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ export * from './time';
 export * from './ability';
 export { default as http } from './http';
 export { default as History } from './history';
+export { default as cache } from './cache';
