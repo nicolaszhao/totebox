abff1862634463950573edc65c46fd23f1a4eba2
赵晓冬 <zhaoxiaodong@hujiang.com>
2018-05-08 18:46:00 +0800

    feat: add array, query to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-export * from './util';
 export * from './string';
+export * from './array';
+export * from './util';
 export * from './time';
 export * from './ability';
+export * from './query';
 export { default as http } from './http';
 export { default as History } from './history';
