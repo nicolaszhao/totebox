ce7a808c2fe2415319e75bb98f694daed701736c
ZhaoXiaodong <zhaoxiaodong@hujiang.com>
2018-06-28 15:22:20 +0800

    feat: add jsonp, date to entry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 export * from './string';
 export * from './array';
 export * from './util';
+export * from './date';
 export * from './time';
 export * from './ability';
 export * from './query';
 export { default as http } from './http';
+export { default as jsonp } from './jsonp';
 export { default as History } from './history';
