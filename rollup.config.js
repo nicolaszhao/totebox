8f711e8e8cd0b080b28bb9593981c873e10c79e5
ZhaoXiaodong <zhaoxd@zenmen.com>
2018-08-27 16:45:04 +0800

    feat: webpack -> rollup

---
+++ rollup.config.js
@@ -0,0 +1,74 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+
+import pkg from './package.json';
+
+const banner = `
+/**
+* NZ's utils
+*
+* Version: ${pkg.version}
+*
+* Author: NZ
+* Web: https://github.com/nicolaszhao/tote-box
+*
+* Licensed under
+*   MIT License http://www.opensource.org/licenses/mit-license
+*
+*/
+`;
+
+const upperCamelCase = (name) => {
+  return name.split('-')
+    .map(text => text.charAt(0).toUpperCase() + text.slice(1))
+    .join('');
+};
+
+const input = 'src/index.js',
+  external = Object.keys(pkg.dependencies);
+
+export default [
+  {
+    input,
+    external,
+    output: {
+      banner,
+      name: upperCamelCase(pkg.name),
+      file: pkg.browser,
+      format: 'umd'
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**'
+      })
+    ]
+  },
+
+  {
+    input, 
+    external,
+    output: {
+      banner,
+      file: pkg.module,
+      format: 'esm'
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**'
+      })
+    ]
+  },
+
+  {
+    input,
+    external,
+    output: {
+      banner,
+      file: pkg.main,
+      format: 'cjs'
+    }
+  }
+];
