0c7985053b00816643aaf12a071a8bfe6fbd5d6f
Nicolas <zhaoxd@zenmen.com>
2019-09-26 20:00:38 +0800

    feat: 重构 timeParser 为 parseTime

--- packages/util/src/time.js
+++ packages/util/src/time.js
@@ -1,98 +1,38 @@
 import { type, noop } from './util';
 
-/**
- * 将提供的毫秒值解析成格式化的时间数值列表
- * @param {Object} options { maxUnit, units }
- *  maxUnit: 'years | months | weeks | days | hours | minutes | seconds'
- *  units: { years: '年', months: '月', ... }
- *
- * @returns (time) => [{ unit: 'Years', value }, ...]
- */
-export function timeParser(options = {}) {
-  const { maxUnit = 'years', units = {} } = options;
-  let unitsSource = [
-    { unit: 'Years', value: 12 },
-    { unit: 'Months', value: 4 },
-    { unit: 'Weeks', value: 7 },
-    { unit: 'Days', value: 24 },
-    { unit: 'Hours', value: 60 },
-    { unit: 'Minutes', value: 60 },
-    { unit: 'Seconds', value: 1000 }
+// 解析给到的毫秒值时间为 { day, hour, minute, second } 的对象
+// options.maxUnit: 最大解析到的时间单位，比如：maxUnit = 'hour'，则不输出 day 的值
+export function parseTime(time, { maxUnit = 'day' } = {}) {
+  const values = {};
+  let source = [
+    { unit: 'day', value: 24 },
+    { unit: 'hour', value: 60 },
+    { unit: 'minute', value: 60 },
+    { unit: 'second', value: 1 },
   ];
-
-  const inAll = (arr, option) => {
-    return arr.reduce((prev, cur) => {
-      if (option === 'add') {
-        return prev + cur;
-      }
-
-      return prev * cur;
-    });
-  };
-
-  const getSourceValues = (units) => {
-    return units.map(unit => unit.value);
-  };
-
-  // 对 unitsSource 数组项增加 unit 代表的毫秒的值
-  unitsSource = unitsSource.map((source, i) => {
-    const key = source.unit.toLowerCase(),
-
-      // 把当前 unit 及之后的 value 从前往后相乘，以获取最终代表的毫秒值
-      ms = inAll(getSourceValues(unitsSource.slice(i)));
-
-    return {
-      ...source,
-      unit: units[key] || source.unit,
-      key,
-      ms
-    };
+  let index = source.findIndex(s => {
+    return s.unit === maxUnit || s.unit.charAt(0) === maxUnit;
   });
 
-  // 从 unitsSource 中过滤掉不需要的数据项
-  let startIndex = unitsSource.findIndex(source => source.key === maxUnit);
-  if (startIndex > 0) {
-    unitsSource = unitsSource.slice(startIndex);
+  if (index > 0) {
+    source = source.slice(index);
   }
 
-  const cache = {};
+  time = Math.round(time / 1000);
 
-  return (time) => {
-    if (type(time) !== 'number' || time < 0) {
-      return unitsSource.map(({ unit }) => ({ value: 0, unit }));
-    }
+  source.forEach((s, i) => {
+    const seconds = source
+      .slice(i)
+      .map(s => s.value)
+      .reduce((a, b) => a * b);
 
-    let values = [];
+    const val = Math.floor(time / seconds);
 
-    unitsSource.reduce((prev, source, i) => {
-      const value = Math.floor(time / unitsSource[i].ms) - prev;
+    time = time - seconds * val;
+    values[s.unit] = val;
+  });
 
-      values.push({ value, unit: source.unit });
-
-      // 算出下一项的值之前的、并与之单位匹配的值的总和，然后在下一次任务中减去该值，比如：
-      // values 为：[{ value: 1, unit: '年' }], 那么 prev 应该为 12（个月）
-      // 在下一次计算 unit: '月' 时，减去这个 prev
-      prev = values.map((n, j) => {
-        let unitValues = cache[`${j}-${i + 1}`];
-
-        // 当 timeParser 初始化后多次调用 (time) => {} 时，可以避免不必要的重复计算
-        if (!unitValues) {
-          unitValues = inAll(getSourceValues(unitsSource.slice(j, i + 1)));
-          cache[`${j}-${i + 1}`] = unitValues;
-        }
-
-        return n.value * unitValues;
-      });
-
-      // 这里，把 prev(array) 每个项上算好的（相对于下一项单位）的所有值相加，比如：
-      // prev: [ 12(个月), 8(个月) ] => 20 个月
-      prev = inAll(prev, 'add');
-
-      return prev;
-    }, 0);
-
-    return values;
-  };
+  return values;
 }
 
 /**
@@ -108,14 +48,13 @@ export function countdown(value, { onStart = noop, onProgress = noop, onEnd = no
 
   const cdType = type(value) === 'date' ? 'date' : 'second';
   const last = value;
-  const times = timeParser({ startUnit: 'hours' });
   let timerId = null;
 
   const tick = () => {
     timerId = setTimeout(() => {
       if (cdType === 'date') {
         const dif = last - Date.now();
-        value = times(dif);
+        value = parseTime(dif);
 
         if (dif <= 0) {
           return onEnd.bind(context)(value);
@@ -136,7 +75,7 @@ export function countdown(value, { onStart = noop, onProgress = noop, onEnd = no
   return {
     start() {
       if (cdType === 'date') {
-        value = times(last - Date.now());
+        value = parseTime(last - Date.now());
       }
 
       onStart.bind(context)(value);
@@ -153,54 +92,13 @@ export function countdown(value, { onStart = noop, onProgress = noop, onEnd = no
   };
 }
 
-// 转换一个毫秒值时间为：hh:mm:ss 格式的字符串，常用于视频、音频时间轴的显示
-export function time2str(time) {
-  let floor = Math.floor,
-    r = [],
-    hour, minute, pad, second;
-
-  time = Math.round(time / 1000);
-  hour = floor(time / 3600);
-  minute = floor((time - 3600 * hour) / 60);
-  second = time % 60;
-
-  pad = function (source, length) {
-    let nagative, pre, str;
-
-    pre = '';
-    nagative = '';
-
-    if (source < 0) {
-      nagative = '-';
-    }
-
-    str = String(Math.abs(source));
-
-    if (str.length < length) {
-      pre = new Array(length - str.length + 1).join('0');
-    }
-
-    return nagative + pre + str;
-  };
-
-  if (hour) {
-    r.push(hour);
-  }
-
-  r.push(pad(minute, 2));
-  r.push(pad(second, 2));
-
-  return r.join(':');
-}
-
 export function isLeapYear(year) {
   return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 }
 
-// format: 'yy-mm-dd'
-// value: '2019-09-25'
-// => (Date) Wed Sep 25 2019 00:00:00 GMT+0800 (中国标准时间)
-export function parseDate(format, value) {
+// example: parseDate('2019-09-25', 'yy-mm-dd');
+// return: (Date) Wed Sep 25 2019 00:00:00 GMT+0800 (中国标准时间)
+export function parseDate(value, format) {
   if (value === '') {
     return null;
   }
@@ -283,7 +181,7 @@ export function parseDate(format, value) {
   return date;
 }
 
-export function formatDate(format, date) {
+export function formatDate(date, format) {
   if (!date) {
     return '';
   }
