423a737ba6848baf96c698e23e8884175a74fd40
Nicolas <zhaoxd@zenmen.com>
2019-09-28 16:46:54 +0800

    chore: update babel config

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,10 @@ module.exports = function(api) {
   api.cache(true);
 
   return {
+    babelrcRoots: [
+      '.',
+      'packages/*',
+    ],
     presets: [
       ['@nicolaz/babel-preset-hamal', { react: false }]
     ]
